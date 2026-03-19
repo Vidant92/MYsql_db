4895a3aa4a2421ded333d9e683ec971c65508966
Vidant92 <vidantbhardwaj92@gmail.com>
2026-03-19 11:35:49 +0530

    kk

---
+++ tablecommands/notes.sql
@@ -0,0 +1,34 @@
+show  databases;
+create database [ dbname];
+use [dbname]; --to switch the databse 
+drop database [ dbname]; -- to deleete the database 
+
+-- create table 
+create table [ table_name](col1 colType (size) extra);
+-- example 
+create table students(id int(9)primary key , name varchar(100)not null, city varchar(500)not null); -- table is created 
+
+-- show tables 
+show tables; -- to show all tables 
+
+-- to describe the tables 
+desc [ table name];
+
+-- table ALTER commands-- (IMPORTANT)
+
+-- add column
+alter table [tablename] ADD [column _name] datatype;
+example -  alter table students add phoneno varchar(10);
+
+-- modify columun
+alter table [tablename] MODIFY[column_name] [newdatatype];
+example - alter table students MODIFY city varchar(300);
+example -  alter table students MODIFY phoneno varchar(15)not null;
+
+-- to RENAME the columnname 
+alter table [tablename] RENAME COLUMN [oldcolumn name ] TO [newcolumn name];
+example-  alter table students RENAME COLUMN phoneno TO studentphone_no;
+
+-- to delete the column ( we use DROP)
+alter table [tablename ] DROP COLUMN [ columnname];
+example-  alter table students DROP COLUMN city;
